d724ceb118497b88605dd473a9b53b7ca0f57956
Erik Kimsey <kimseyerik@gmail.com>
2019-10-06 17:01:38 -0400

    Creating global store with Context and Hooks

--- components/emergency/ErQuestionItem_bool.js
+++ components/emergency/ErQuestionItem_bool.js
@@ -1,43 +1,14 @@
-// import React from 'react';
-// import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text } from 'react-native';
 
-// const ErQuestionItemBool = (props) => {
-//   // console.log(props);
+const ErQuestionItemBool = (props) => {
+  // console.log(props);
 
-//   let answerField = {
-//     <View>
-//       <Text>{}</Text>
-//     </View>
-//   }
+  return (
+    <View>
+      <Text>{props.question.question}</Text>
+    </View>
+  );
+};
 
-//   if(props.question.answerType === 'bool'){
-//     return (
-//       <View>
-//         <Text>{props.question.question}</Text>
-//       </View>
-//     );
-
-//   } if else (props.question.answerType === 'string'){
-//       return (
-//         <View>
-//           <Text>{props.question.question}</Text>
-//         </View>
-//       );
-
-//     } if else (props.question.answerType === 'array'){
-//         return (
-//           <View>
-//             <Text>{props.question.question}</Text>
-//           </View>
-//         );
-//       } else {
-//           return (
-//             <View>
-//               <Text>{props.question.question}</Text>
-//             </View>
-//           );
-
-//   }
-// };
-
-// export default ErQuestionItemBool;
+export default ErQuestionItemBool;
