210ab2e07554fc8fd19d0413bb7adfb646c4f057
Erik Kimsey <kimseyerik@gmail.com>
2019-10-06 17:50:20 -0400

    Adjusted id for use with Context API

--- questionnaires/emergency_questions.js
+++ questionnaires/emergency_questions.js
@@ -1,36 +1,36 @@
 const emergency_qs = [
   {
-    id: 1,
+    id: 'ER_1',
     question: 'Can you answer a few, quick questions about the anxiety you are experiencing?',
     answerType: 'bool',
     answer: null
   },
   {
-    id: 2,
+    id: 'ER_2',
     question: 'What is the location where are you experiencing anxiety right now?',
     answerType: 'string',
     answer: ''
   },
   {
-    id: 3,
+    id: 'ER_3',
     question: 'What specific aspects, of your environment, triggered your anxiety? ',
     answerType: 'string',
     answer: ''
   },
   {
-    id: 4,
+    id: 'ER_4',
     question: 'Are you afraid of being judged?',
     answerType: 'bool',
     answer: null
   },
   {
-    id: 5,
+    id: 'ER_5',
     question: 'Can you accept that your fear of being judged is irrational?',
     answerType: 'bool',
     answer: null
   },
   {
-    id: 6,
+    id: 'ER_6',
     question: 'Think of three different words that describe how you feel, right now?',
     answerType: 'array',
     answer: []
