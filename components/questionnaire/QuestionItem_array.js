bc4d762d09ee474dab423c1ef697abf27cdde02f
Erik Kimsey <kimseyerik@gmail.com>
2019-10-16 11:57:06 -0400

    Alert displayed when ER_Q length is not 3

--- components/questionnaire/QuestionItem_array.js
+++ components/questionnaire/QuestionItem_array.js
@@ -1,5 +1,5 @@
 import React, { Component, useContext } from 'react';
-import { View, Text, TextInput, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Alert, Text, TextInput, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import Modal from '../../utility/modal';
 
 import generalQItemStyles from '../questionnaire/styles/questionItem_styles';
@@ -26,7 +26,11 @@ export default class QuestionItemArray extends Component {
     console.log('answerArr');
     console.log(answerArr);
 
-    this.props.setAnswer({ id: this.props.qObj.id, answer: answerArr });
+    if(answerArr != null){
+      this.props.setAnswer({ id: this.props.qObj.id, answer: answerArr });
+    } else {
+      return;
+    }
   };
 
   createArrayFromString = () => {
@@ -37,12 +41,30 @@ export default class QuestionItemArray extends Component {
       console.log('======');
       console.log('call alert modal');
       console.log('======');
-      return;
+      this.callAlert();
+      return null;
     }
   };
 
   checkArrayLength = (arr) => {
     return arr.length === 3 ? true : false;
+  };
+
+  callAlert = () => {
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel'
+        },
+        { text: 'OK', onPress: () => console.log('OK Pressed') }
+      ],
+      { cancelable: false }
+    );
   };
 
   render() {
