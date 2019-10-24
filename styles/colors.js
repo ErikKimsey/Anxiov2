c374dba7faab36509d58521f5f44530c29226ba9
Erik Kimsey <kimseyerik@gmail.com>
2019-10-24 11:20:56 -0400

    Added alternative palette

--- styles/colors.js
+++ styles/colors.js
@@ -6,5 +6,20 @@ const COLORS = {
   brown: '#593c2c',
   erRed: '#fc385f'
 };
+/** */
+/* Alternative Palette */
+// @For-Charts-&-Graphs-1-hex: #013859;
+// @For-Charts-&-Graphs-2-hex: #C4BFB3;
+// @For-Charts-&-Graphs-3-hex: #3F2459;
+// @For-Charts-&-Graphs-4-hex: #8C3D69;
+// @For-Charts-&-Graphs-5-hex: #D82479;
+
+/* Color Theme Swatches in RGBA */
+// @For-Charts-&-Graphs-1-rgba: rgba(1, 56, 89, 1);
+// @For-Charts-&-Graphs-2-rgba: rgba(196, 191, 179, 1);
+// @For-Charts-&-Graphs-3-rgba: rgba(63, 36, 89, 1);
+// @For-Charts-&-Graphs-4-rgba: rgba(140, 61, 105, 1);
+// @For-Charts-&-Graphs-5-rgba: rgba(216, 36, 121, 1);
+/** */
 
 export default COLORS;
