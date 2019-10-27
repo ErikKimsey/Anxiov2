28d7692d71b6a302cfd91372a6fe81877ca27e62
Erik Kimsey <kimseyerik@gmail.com>
2019-10-27 11:19:31 -0400

    trying to determine how to prevent app from closing when user presses device backbutton

--- navigation/screen_navigation/EmergencyNav.js
+++ navigation/screen_navigation/EmergencyNav.js
@@ -1,15 +1,27 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from '../../screens/HomeScreen';
 import EmergencyScreen from '../../screens/EmergencyScreen';
-import COLORS, { COLORS_2 } from '../../styles/colors';
+import { SCREENS } from '../../screens/index';
 
 const EmergencyNav = createStackNavigator(
   {
-    Home: {
-      screen: HomeScreen
-    },
     Emergency: {
       screen: EmergencyScreen
+    },
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        header: null
+      }
+    },
+    Breathing: {
+      screen: SCREENS[0].screen
+    },
+    Data: {
+      screen: SCREENS[1].screen
+    },
+    Bubble: {
+      screen: SCREENS[2].screen
     }
   },
   {
