026057fdd2a559b26bfb194da46715b96597ea12
Erik Kimsey <kimseyerik@gmail.com>
2019-10-31 14:08:11 -0400

    Dynamic string functionality working

--- components/questionnaire/QuestionItem_bool.js
+++ components/questionnaire/QuestionItem_bool.js
@@ -1,65 +1,89 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, Component } from 'react';
 import { View, Text, TextInput, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import questionItemStyles from '../questionnaire/styles/questionItem_styles';
 import COLORS, { COLORS_2 } from '../../styles/colors';
 // import textTransition, { Q, getChars } from '../../utility/textTransition';
 
-const textTransition = (str, setStr, Q, i) => {
-  const STR_LEN = str.length;
-  loop(str, STR_LEN, setStr, Q);
-};
+export default class QuestionItemBool extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: null,
+      answer: null,
+      qToBuild: ''
+    };
+  }
 
-const loop = (str, len, i = 0, setStr, Q) => {
-  let _i = i;
-  let char = str.charAt(_i);
-  setTimeout(() => {
-    _i++;
-    if (_i < len) {
-      loop(str, len, _i);
-    }
+  componentDidMount() {
+    let { question, answer } = this.props.qObj;
+
+    this.setState({ question: question, answer: answer });
+    // let testQ = this.props.qObj.question;
+    this.textTransition(question);
+  }
+
+  textTransition = (str) => {
+    STR_LEN = str.length;
+    this.loop(str, STR_LEN);
+  };
+
+  loop = (str, len, i = 0) => {
+    let _i = i;
+    let char = str.charAt(_i);
+    this.setChars(char);
+    setTimeout(() => {
+      _i++;
+      if (_i < len) {
+        this.loop(str, len, _i);
+      }
+    }, 20);
+  };
+
+  setChars = (char) => {
     // console.log(char);
-    Q += char;
-    setStr(Q);
-  }, 300);
-};
+    // let nuQ;
+    console.log('state');
 
-const getChars = (char) => {
-  return Q;
-};
+    console.log(this.state.qToBuild);
+    console.log('char');
+    console.log(char);
 
-const QuestionItemBool = (props) => {
-  let { question, answer } = props.qObj;
-  let { str, setStr } = useState('');
-  console.log(str);
+    if (this.state.qToBuild === '') {
+      this.setState({ qToBuild: char });
+    } else {
+      let nuQ = this.state.qToBuild.slice();
+      console.log(nuQ);
+      this.state.qToBuild.concat(char);
+      this.setState({ qToBuild: this.state.qToBuild.concat(char) });
+    }
+  };
 
-  const testQ = props.qObj.question;
-  textTransition(testQ);
-
-  return (
-    <View style={[ styles.container ]}>
-      <Text style={styles.questionText}>{question}</Text>
-      <View style={styles.answerButtonsContainer}>
-        <TouchableOpacity
-          // style={[ styles.answerButtons ]}
-          onPress={() => {
-            props.setAnswers({ id: props.qObj.id, answer: true });
-          }}
-        >
-          <Text style={styles.answerButtonsText}>Yes</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            props.setAnswers({ id: props.qObj.id, answer: false });
-          }}
-        >
-          <Text style={styles.answerButtonsText}>Nope</Text>
-        </TouchableOpacity>
+  render() {
+    return (
+      <View style={[ styles.container ]}>
+        <Text style={styles.questionText}>{this.state.qToBuild}</Text>
+        <View style={styles.answerButtonsContainer}>
+          <TouchableOpacity
+            // style={[ styles.answerButtons ]}
+            onPress={() => {
+              this.props.setAnswers({ id: this.props.qObj.id, answer: true });
+            }}
+          >
+            <Text style={styles.answerButtonsText}>Yes</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              this.props.setAnswers({ id: this.props.qObj.id, answer: false });
+            }}
+          >
+            <Text style={styles.answerButtonsText}>Nope</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
+    );
+  }
+}
+styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS_2.gray,
@@ -87,5 +111,3 @@ const styles = StyleSheet.create({
     color: COLORS.creme
   }
 });
-
-export default QuestionItemBool;
